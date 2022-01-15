69cc62bc56d616923d0a959263ed814862aa2737
Jayfunf <simh3077@gmail.com>
2022-01-15 19:49:26 +0900

    add: diartStore for Store text data & mainView for user's view

---
+++ iOS_Android_Diary/iOS_Android_Diary/Model/diaryStore.swift
@@ -0,0 +1,40 @@
+//
+//  diaryStore.swift
+//  iOS_Android_Diary
+//
+//  Created by Minhyun Cho on 2022/01/12.
+//
+
+import Foundation
+
+class diaryStore: ObservableObject{
+    @Published var DiaryList: [Diary]
+    
+    
+    init() {
+        DiaryList = [
+            Diary(content: "Test String1"),
+            Diary(content: "Test String2"),
+            Diary(content: "Test String3")
+        ]
+    }
+    
+    func insert(memo: String) {
+        DiaryList.insert(Diary(content: memo), at: 0)
+    }
+    
+    func update(memo: Diary?, content: String) {
+        guard let memo = memo else { return }
+        memo.content = content
+    }
+    
+    func delete (memo: Diary) {
+        self.DiaryList.removeAll()
+    }
+    
+    func delete(set: IndexSet) {
+        for index in set {
+            self.DiaryList.remove(at: index)
+        }
+    }
+}
---
+++ iOS_Android_Diary/iOS_Android_Diary/View/mainView.swift
@@ -0,0 +1,20 @@
+//
+//  mainView.swift
+//  iOS_Android_Diary
+//
+//  Created by Minhyun Cho on 2022/01/15.
+//
+
+import SwiftUI
+
+struct mainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct mainView_Previews: PreviewProvider {
+    static var previews: some View {
+        mainView()
+    }
+}
